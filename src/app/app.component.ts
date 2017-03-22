deff6e4a83814bd594ae83558a782ac3d26c7bbf
Glenn Greibesland <glenn@glenng.no>
2017-03-22 21:17:37 +0100

    use geolocation service to center map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { GeolocationService } from './geolocation/geolocation.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +11,13 @@ export class AppComponent {
   title: string = 'Oversikt over parkeringsplasser';
   lat: number = 58.145975;
   lng: number = 7.985508;
+
+  constructor(private geoLocationService: GeolocationService) { }
+
+  onButtonClick() {
+    this.geoLocationService.getCurrentPosition().subscribe((pos) => {
+        this.lat = pos.lat;
+        this.lng = pos.long;
+    });
+  }
 }
