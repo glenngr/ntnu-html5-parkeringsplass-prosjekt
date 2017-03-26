808d7831fb4209157f085ea02ca7fee165a0c609
Glenn Greibesland <glenn@glenng.no>
2017-03-26 19:54:10 +0200

    make parking space radius an @Input

--- src/app/parking-space-map/parking-space-info-circle/parking-space-info-circle.component.ts
+++ src/app/parking-space-map/parking-space-info-circle/parking-space-info-circle.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ParkingSpaceInfoCircleComponent implements OnInit {
   @Input() latitude: number;
   @Input() longitude: number;
+  @Input() parkingSpaceRadius: number = 100;
   @Input() freeSpaces: number;
   @Input() totalSpaces: number;
   @Input() parkingSpaceName: string;
   circleFillColor: string = 'red';
-  circleRadius: number = 300;
   
   constructor() { }
 
