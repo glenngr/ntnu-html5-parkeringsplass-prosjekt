b85733ce6a1871c6c130caefc00035605cf574b9
Glenn Greibesland <glenn@glenng.no>
2017-03-28 20:16:10 +0200

    rename module file to index.ts

--- src/app/parking-space-filter/index.ts
+++ src/app/parking-space-filter/index.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+
 import { ParkingSpaceFilterComponent } from './parking-space-filter.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
   ],
+  exports: [ParkingSpaceFilterComponent],
   declarations: [ParkingSpaceFilterComponent]
 })
 export class ParkingSpaceFilterModule { }
