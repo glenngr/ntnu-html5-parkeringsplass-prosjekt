298eadcd1f469908148154cede19f0b29041fe55
Glenn Greibesland <glenn@glenng.no>
2017-03-29 20:56:23 +0200

    add hourlyRate and description props to ParkingSpace

--- src/app/parking-space-map/models/parkingspace.model.ts
+++ src/app/parking-space-map/models/parkingspace.model.ts
@@ -1,5 +1,13 @@
 import { Location } from './location.model';
 
 export class ParkingSpace {
-    constructor(public name: string, public totalSpaces: number, public occupiedSpaces: number, public location: Location, public size: number) { }
+    constructor(
+        public name: string,
+        public totalSpaces: number,
+        public occupiedSpaces: number,
+        public location: Location,
+        public size: number,
+        public hourlyRate: number,
+        public description: string
+    ) { }
 }
