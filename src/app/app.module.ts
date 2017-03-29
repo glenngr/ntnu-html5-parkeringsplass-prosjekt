d509c759c72a9c39a2de57b760edb843ca6f4103
Glenn Greibesland <glenn@glenng.no>
2017-03-29 20:07:24 +0200

    add and register angular2-localstorage package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import {WebStorageModule, LocalStorageService, WEBSTORAGE_CONFIG } from 'angular2-localstorage';
+WEBSTORAGE_CONFIG.prefix = '';
 
 import { AppComponent } from './app.component';
 import { ChooseThemeDialogComponent } from './choose-theme-dialog/choose-theme-dialog.component';
@@ -22,7 +24,11 @@ import { ParkingSpaceMapModule } from './parking-space-map/';
     FormsModule,
     HttpModule,
     MaterialModule,
+    WebStorageModule,
     ParkingSpaceMapModule,
+  ],
+  providers: [
+    LocalStorageService,
   ],
   bootstrap: [AppComponent]
 })
