935bb07ae68c4485be06d442db93201ae9b2d34e
Glenn Greibesland <glenn@glenng.no>
2017-03-29 19:38:04 +0200

    finished creating choose-theme-dialog

--- src/app/choose-theme-dialog/choose-theme-dialog.component.ts
+++ src/app/choose-theme-dialog/choose-theme-dialog.component.ts
@@ -2,9 +2,24 @@ import { Component, ViewChild } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
-  selector: 'app-theme-dialog',
-  templateUrl: './choose-theme-dialog.component.html',
+    selector: 'app-theme-dialog',
+    templateUrl: './choose-theme-dialog.component.html',
+    styleUrls: ['choose-theme-dialog.component.css']
 })
 export class ChooseThemeDialogComponent {
-  constructor(public dialogRef: MdDialogRef<ChooseThemeDialogComponent>) {}
+    selectedTheme: string;
+    themes: string[] = [];
+    private themeDict = {};
+
+    constructor(public dialogRef: MdDialogRef<ChooseThemeDialogComponent>) {
+        this.themeDict['Default theme'] = 'default';
+        this.themeDict['Dark theme'] = 'dark-theme';
+        this.themeDict['Unicorn dark theme'] = 'unicorn-dark-theme';
+        this.themeDict['Deep purple theme'] = 'deep-purple-theme';
+        for (let property in this.themeDict) {
+            if (this.themeDict.hasOwnProperty(property)) {
+                this.themes.push(property);
+            }
+        }
+    }
 }
