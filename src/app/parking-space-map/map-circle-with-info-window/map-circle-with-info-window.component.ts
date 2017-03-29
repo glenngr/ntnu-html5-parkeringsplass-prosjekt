b18b96c16f9da424b2376aa52b8a6a6f0ced8150
Glenn Greibesland <glenn@glenng.no>
2017-03-29 20:54:54 +0200

    Expose circleClick @Output

--- src/app/parking-space-map/map-circle-with-info-window/map-circle-with-info-window.component.ts
+++ src/app/parking-space-map/map-circle-with-info-window/map-circle-with-info-window.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
+import { SebmGoogleMapInfoWindow } from 'angular2-google-maps/core';
 
 @Component({
   selector: 'gg-map-circle-with-info-window',
@@ -12,10 +13,17 @@ export class MapCircleWithInfoWindowComponent implements OnInit {
   @Input() circleFillColor: string;
   @Input() circleRadius: number;
   @Input() infoWindowOpen: boolean;
+  @Output() circleClick: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild(SebmGoogleMapInfoWindow) infoWindow: SebmGoogleMapInfoWindow;
 
   constructor() { }
 
-  ngOnInit() {
+  onCircleClick() {
+    this.infoWindow.open();
+    this.infoWindowOpen = this.infoWindow.isOpen;
+    this.circleClick.emit();
   }
 
+  ngOnInit() {
+  }
 }
