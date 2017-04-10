6ee24ffa0b312b56f7deff2a2dc7403cec9164d3
Glenn Greibesland <glenn@glenng.no>
2017-04-10 16:23:59 +0200

    added app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ParkingSpaceMapComponent } from './parking-space-map/parking-space-map.component';
-
+import { ParkingSpaceMapHomeComponent } from './parking-space-map/parking-space-map-home.component';
 const appRoutes: Routes = [
-    { path: '', component: ParkingSpaceMapComponent, pathMatch: 'full' },
+    { path: '', component: ParkingSpaceMapHomeComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -20,10 +20,3 @@ const appRoutes: Routes = [
     ]
 })
 export class AppRoutingModule { }
-
-
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
