263ddc49f7a75a991963b39423e0fa7d6ae20667
Glenn Greibesland <glenn@glenng.no>
2017-04-10 21:12:38 +0200

    Show a button for each parking space and show history on button click

--- src/app/parking-space-map/parking-space-stats/parking-space-stats.component.ts
+++ src/app/parking-space-map/parking-space-stats/parking-space-stats.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { ParkingSpaceBackendService } from '../parking-space-backend-service/';
+import { HistoryCollection } from '../models/history-collection.model';
 
 @Component({
   selector: 'app-parking-space-stats',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parking-space-stats.component.css']
 })
 export class ParkingSpaceStatsComponent implements OnInit {
+  parkingSpaces: Observable<string[]>;
+  historyData: HistoryCollection;
+  loadingHistory: boolean;
+  private historyDataSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private backendService: ParkingSpaceBackendService,
+  ) { }
 
   ngOnInit() {
+    this.parkingSpaces = this.backendService.getParkingSpaceList();
   }
 
+  onPsButtonClick(paringSpaceName) {
+    console.log(paringSpaceName);
+    this.loadingHistory = true;
+    if (this.historyDataSubscription) {
+      this.historyDataSubscription.unsubscribe();
+    }
+
+    this.historyDataSubscription = this.backendService
+      .getHistory(paringSpaceName)
+      .subscribe(data => {
+        this.historyData = data;
+        this.loadingHistory = false;
+      });
+  }
 }
