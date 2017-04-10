4a47b927d468ff23e5d9cb55c6b8eeff19b388b9
Glenn Greibesland <glenn@glenng.no>
2017-04-10 16:23:35 +0200

    add parking-space-home component

---
+++ src/app/parking-space-map/parking-space-map-home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-parking-space-map-home',
+    templateUrl: 'parking-space-map-home.component.html'
+})
+
+export class ParkingSpaceMapHomeComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
