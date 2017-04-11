5e8ba1c4b1608f0c4243c23e618e739bcf58c023
Glenn Greibesland <glenn@glenng.no>
2017-04-11 09:21:42 +0200

    Add BarChart-models file with BarChartData class

---
+++ src/app/parking-space-map/parking-space-stats/barchart-models.ts
@@ -0,0 +1,8 @@
+export class BarChartData {
+    constructor(
+        public name: string,
+        public value: number,
+        public valueType: string,
+        public barColor: string,
+    ) { }
+}
