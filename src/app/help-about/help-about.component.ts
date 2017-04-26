4dffa710ddf17a4e67033768916b3d401d1ff721
Glenn Greibesland <glenn@glenng.no>
2017-04-26 20:55:44 +0200

    inject APP_BASE_HREF into help-about component, use configured base url for assets

--- src/app/help-about/help-about.component.ts
+++ src/app/help-about/help-about.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-help-about',
   templateUrl: './help-about.component.html',
   styleUrls: ['./help-about.component.css']
 })
-export class HelpAboutComponent implements OnInit {
+export class HelpAboutComponent {
+  private _assetsUrl: string;
+  private _statsNavigateHowtoGif = 'stats-navigate-howto.gif';
+  private _filterHowtoGif = 'filter-howto.gif';
+  private _mylocationHowtoGif = 'mylocation-howto.gif';
+  private _detailsHowtoGif = 'details-howto.gif';
+  private _statsChooseHowtoGif = 'stats-choose-howto.gif';
+  private _statsColorHowtoGif = 'stats-color-howto.gif';
+  private _statsPercentHowtoGif = 'stats-percent-howto.gif';
+  private _themeHowtoGif = 'theme-howto.gif';
 
-  constructor() { }
-
-  ngOnInit() {
+  get themeHowtoGif() {
+    return this._assetsUrl + this._themeHowtoGif;
   }
 
+  get statsPercentHowtoGif() {
+    return this._assetsUrl + this._statsPercentHowtoGif;
+  }
+
+  get statsColorHowtoGif() {
+    return this._assetsUrl + this._statsColorHowtoGif;
+  }
+
+  get statsChooseHowtoGif() {
+    return this._assetsUrl + this._statsChooseHowtoGif;
+  }
+
+  get detailsHowtoGif() {
+    return this._assetsUrl + this._detailsHowtoGif;
+  }
+
+  get mylocationHowtoGif() {
+    return this._assetsUrl + this._mylocationHowtoGif;
+  }
+
+  get filterHowtoGif() {
+    return this._assetsUrl + this._filterHowtoGif;
+  }
+
+  get statsNavigateHowtoGif() {
+    return this._assetsUrl + this._statsNavigateHowtoGif;
+  }
+  constructor( @Inject(APP_BASE_HREF) private baseHref: string) {
+    console.log('configured basehref:', this.baseHref);
+    this._assetsUrl = this.baseHref + 'assets/';
+  }
 }
