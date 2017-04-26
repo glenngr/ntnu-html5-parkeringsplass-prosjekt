93761da38046f685933e1e7d8eadefd54ae6770d
Glenn <glenn@glenng.no>
2017-04-26 19:08:06 +0200

    removed redundant code

--- src/app/parking-space-overview/parking-space-overview.component.ts
+++ src/app/parking-space-overview/parking-space-overview.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-parking-space-overview',
     templateUrl: 'parking-space-overview.component.html'
 })
 
-export class ParkingSpaceMapHomeComponent implements OnInit {
-
-    constructor() { }
-
-    ngOnInit() { }
+export class ParkingSpaceMapHomeComponent {
 }
